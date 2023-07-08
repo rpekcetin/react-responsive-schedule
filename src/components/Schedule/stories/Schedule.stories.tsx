0fdf3907ccaaf43ae9fc72cb4d7f66d0dc31d907
RPEKCETIN <receppek38@gmail.com>
2023-07-09 01:30:58 +0300

    types changed

--- src/components/Schedule/stories/Schedule.stories.tsx
+++ src/components/Schedule/stories/Schedule.stories.tsx
@@ -4,9 +4,19 @@ import Schedule from '../main/Schedule';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: any = {
-    title: 'Xyz/Schedule',
+    title: 'Components/Schedule',
     component: Schedule,
     tags: ['autodocs'],
+    argTypes: {
+        currentMonthProp: { description: 'Handle current month with react hooks.', defaultValue: null, control: { type: null } },
+        setCurrentMonthProps: { control: { type: null } },
+        onClickClear: { table: { disable: true }, control: { type: null } },
+        onClickAdd: { control: { type: null } },
+        onClickClose: { control: { type: null } },
+        onClickDay: { control: { type: null } },
+        onClickPreviousMonth: { control: { type: null } },
+        onClickNextMonth: { control: { type: null } },
+    }
 } satisfies Meta<typeof Schedule>;
 
 export default meta;
@@ -15,7 +25,5 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const World: Story = {
     args: {
-        label: 'Hello World',
-        children: (<div>lebogle</div>)
     },
 };
--- src/components/Schedule/types/types.ts
+++ src/components/Schedule/types/types.ts
@@ -14,21 +14,6 @@ export interface IOnClickRemoveNote {
   date: string | undefined;
 }
 export interface IScheduleProps {
-  label: string | undefined;
-  id?: string | undefined;
-  //children?: string | JSX.Element | JSX.Element[] | undefined
-  className?: string | undefined;
-  p?: string | undefined;
-  pr?: string | undefined;
-  pl?: string | undefined;
-  pt?: string | undefined;
-  pb?: string | undefined;
-  m?: string | undefined;
-  mr?: string | undefined;
-  ml?: string | undefined;
-  mt?: string | undefined;
-  mb?: string | undefined;
-  style?: React.CSSProperties | undefined;
   currentMonthProp?: ICurrentMontProp;
   setCurrentMonthProps?: React.Dispatch<React.SetStateAction<ICurrentMontProp>>;
   onClickAdd?: Function;
